8472cbddda398ae6307357448fb36c099c9c0f3d
Antonio <antoniowyep@gmail.com>
2017-06-29 15:05:20 -0400

    working alexa play/stop/what song, stub code for add to favorite

---
+++ alexa wyep skill.js
@@ -0,0 +1,225 @@
+/**
+ 
+ Copyright 2017 Antonio Licon.
+ 
+*/
+
+ 'use strict';
+const https = require("https");
+
+exports.handler = function (event, context) {
+    try {
+		 
+     if (event.session.application.applicationId !== "amzn1.ask.skill.92cdd6ec-a294-432e-9df5-c9b8f024ab7e") {
+         context.fail("Invalid Application ID");
+     }
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+				switch (event.request.type) {
+					case "LaunchRequest":
+						onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+						break;
+					case "IntentRequest":
+						onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+						break;
+					...
+					case "SessionEndedRequest":
+						onSessionEnded(event.request, event.session);
+            context.succeed();
+						break;
+					default:
+						break;
+				}
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // add any session init logic here
+}
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var cardTitle = "W. Y. E. P. Where the music matters";
+    var speechOutput = "Commands you can say are: Alexa, tell y. e. p. to play.  Alexa, tell y. e. p. to stop.  Alexa, ask y. e. p. what song this is. Alexa tell y. e. p. to favorite.";
+    callback(session.attributes,
+        buildSpeechletResponse(cardTitle, speechOutput, "", true));
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // dispatch custom intents to handlers here
+    if (intentName == 'play') {
+        play(intent, session, callback);
+    }
+    else if (intentName == 'stop') {
+        stop(intent, session, callback);
+    }
+		else if (intentName == 'whatSong') {
+        whatSong(intent, session, callback);
+    }
+		else if (intentName == 'favorite') {
+        favorite(intent, session, callback);
+    }
+    else {
+        throw "Invalid intent";
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // Add any cleanup logic here
+}
+
+function whatSong(intent, session, callback) {
+    var surl = 'https://api.composer.nprstations.org/v1/widget/50e451b6a93e91ee0a00028e/now?format=json';
+
+		https.get(surl, function(res){
+				var body = '';
+
+				res.on('data', function(chunk){
+						body += chunk;
+				});
+
+				res.on('end', function(){
+						var sresponse = JSON.parse(body);
+						var songInfo="";
+						if (sresponse.onNow.song.trackName === ""){
+							 songInfo = "I'm sorry, the song information is missing";
+						} else {
+							songInfo = "This song is: " + sresponse.onNow.song.trackName + " by " + sresponse.onNow.song.artistName;
+							}
+						callback(session.attributes, buildSpeechletResponseWithoutCard(songInfo, "", "true"));
+						});
+		}).on('error', function(e){
+					callback(session.attributes, buildSpeechletResponseWithoutCard("There was an error, please try again later", "", "true"));
+		});
+		// callback(session.attributes, buildSpeechletResponseWithoutCard("This song is feel it still by portugal the man", "", "true"));
+}
+
+function favorite(intent, session, callback) {
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("This song was added to your favorites", "", "true"));
+}
+
+// ------- Helper functions to build responses -------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
+
+function stop(intent, session, callback) {
+    var response = {
+        version: "1.0",
+        response: {
+            shouldEndSession: true,
+            directives: [
+                {
+                    "type": "AudioPlayer.Stop"
+                }
+            ]
+        }
+    };
+    callback(session.attributes,response.response);
+} 
+
+function play(intent, session, callback) {
+       var response = {
+        version: "1.0",
+        response: {
+            shouldEndSession: true,
+            directives: [
+                {
+                    type: "AudioPlayer.Play",
+                    playBehavior: "REPLACE_ALL", 
+                    audioItem: {
+                        stream: {
+                            url: "https://playerservices.streamtheworld.com/api/livestream-redirect/WYEPFMAAC.aac",
+                            token: "913", 
+                            expectedPreviousToken: null, 
+                            offsetInMilliseconds: 0
+                        }
+                    }
+                }
+            ]
+        }
+    };
+callback(session.attributes, response.response);
+}
